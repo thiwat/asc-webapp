22d70c40a66a84acb86ee097017339645af2913f
Thiwat Nuntjaruwong <thiwat@shopstack.asia>
2024-10-05 01:56:54 +0700

    Fix style

--- src/components/ui/BottomSheet/Ticket/index.tsx
+++ src/components/ui/BottomSheet/Ticket/index.tsx
@@ -23,44 +23,46 @@ const TicketBottomSheet = ({
       <Sheet.Container className={'!rounded-t-3xl'}>
         <Sheet.Header style={{ marginTop: -30, marginBottom: -10 }} />
         <Sheet.Content>
-          <img
-            src={data?.event?.cover?.url}
-            className={'w-full rounded-t-3xl'}
-          />
-          <div className={'p-3'}>
-            <Typography type={'title'}>
-              {data?.event?.name}
-            </Typography>
-            <div className={'flex flex-row space-x-3 items-center mt-3'}>
-              <Badge
-                label={dayjs(data?.event?.event_start_date).format('DD')}
-                sublabel={dayjs(data?.event?.event_start_date).format('MMM')}
-              />
-              <div className={'space-y-1'}>
-                <div className={'flex flex-row items-center space-x-2'}>
-                  <ClockIcon height={18} width={18} />
-                  <div className={'text-sm'}>{`${dayjs(data?.event?.event_start_date).format('HH:mm')} - ${dayjs(data?.event?.event_end_date).format('HH:mm')}`}</div>
-                </div>
-                <div className={'flex flex-row items-center space-x-2'}>
-                  <MapIcon height={18} width={18} />
-                  <div className={'text-sm'}>{data?.event?.location}</div>
+          <Sheet.Scroller draggableAt="both">
+            <img
+              src={data?.event?.cover?.url}
+              className={'w-full rounded-t-3xl'}
+            />
+            <div className={'p-3'}>
+              <Typography type={'title'}>
+                {data?.event?.name}
+              </Typography>
+              <div className={'flex flex-row space-x-3 items-center mt-3'}>
+                <Badge
+                  label={dayjs(data?.event?.event_start_date).format('DD')}
+                  sublabel={dayjs(data?.event?.event_start_date).format('MMM')}
+                />
+                <div className={'space-y-1'}>
+                  <div className={'flex flex-row items-center space-x-2'}>
+                    <ClockIcon height={18} width={18} />
+                    <div className={'text-sm'}>{`${dayjs(data?.event?.event_start_date).format('HH:mm')} - ${dayjs(data?.event?.event_end_date).format('HH:mm')}`}</div>
+                  </div>
+                  <div className={'flex flex-row items-center space-x-2'}>
+                    <MapIcon height={18} width={18} />
+                    <div className={'text-sm'}>{data?.event?.location}</div>
+                  </div>
                 </div>
               </div>
+              <div className={'mt-6 mb-12'}>
+                <Slider
+                  dots
+                  infinite={false}
+                >
+                  {(data.tickets || []).map(i => (
+                    <QRCode
+                      key={i.code}
+                      value={i.code}
+                    />
+                  ))}
+                </Slider>
+              </div>
             </div>
-            <div className={'mt-6 mb-12'}>
-              <Slider
-                dots
-                infinite={false}
-              >
-                {(data.tickets || []).map(i => (
-                  <QRCode
-                    key={i.code}
-                    value={i.code}
-                  />
-                ))}
-              </Slider>
-            </div>
-          </div>
+          </Sheet.Scroller>
         </Sheet.Content>
       </Sheet.Container>
     </Sheet>
--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,9 +1,43 @@
+import Link from "next/link";
 import { HeaderProps } from "./types";
+import { useRouter } from "next/router";
+import {
+  ListBulletIcon,
+  TicketIcon
+} from '@heroicons/react/24/outline'
 
 const Header = ({ }: HeaderProps) => {
+
+  const router = useRouter()
+
+  if (router.pathname == '/[key]') {
+    return (
+      <div className={'flex flex-row justify-center items-center text-primary font-bold text-2xl py-2'}>
+        <div>ASC</div>
+      </div>
+    )
+  }
+
   return (
     <div className={'flex flex-row justify-center items-center text-primary font-bold text-2xl py-2'}>
-      <div>ASC</div>
+      <div className={'flex-1'} />
+      <Link href={'/tickets'}>
+        <div>ASC</div>
+      </Link>
+      <div className={'flex flex-1 flex-row justify-end items-center pr-3 space-x-3'}>
+        <Link href={'/orders'}>
+          <ListBulletIcon
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link href={'/tickets'}>
+          <TicketIcon
+            width={24}
+            height={24}
+          />
+        </Link>
+      </div>
     </div>
   )
 }
